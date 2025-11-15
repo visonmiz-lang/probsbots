2a5502486291004309b647ccd7663f4aed280ca0
visonmiz-lang <visonmiz@gmail.com>
2025-11-15 21:55:04 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: false
-  }
+  // Убери experimental.turbo полностью
 };
 
 export default nextConfig;
